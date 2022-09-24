d65fd6cb7e7ce062127fc21949877477050bc7dc
entiar <87098963+entiar@users.noreply.github.com>
2022-09-24 10:44:32 -0400

    Create java1.java

---
+++ java1.java
@@ -0,0 +1,30 @@
+package com.variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        /**
+         * int
+         * long
+         * float
+         * double
+         * boolean
+         * String
+         *
+         */
+        int entero = 30;
+        long masEntero = 2022;
+        float decimal = 3.1416f;
+        double decimalG = 1.2356343d;
+        boolean si = true;
+        String cadena = "Es una texto";
+
+        System.out.println("Este es un numero entero o int --> " + entero);
+        System.out.println("Este es un numero largo o long --> " + masEntero);
+        System.out.println("Este es un número flotante o float --> " + decimal);
+        System.out.println("Este es un número doble o double --> " + decimalG);
+        System.out.println("Este es un valor booleano o boolean --> " + si);
+        System.out.println("Este es un valor de cadena o string --> " + cadena);
+
+    }
+}
+
