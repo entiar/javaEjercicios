19106f66c26c48f59ed09664959b585cf7a9f77b
entiar <87098963+entiar@users.noreply.github.com>
2022-09-26 20:03:41 -0400

    Create java2.java

---
+++ java2.java
@@ -0,0 +1,15 @@
+package com.date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(clase(0.33));
+
+
+    }
+    static double clase(double iva){
+        double presio = 222.3;
+        return presio + (presio*iva);
+    }
+
+
+}
